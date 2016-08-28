6a2c4f070551afb01ed81962e8b041d4a1f22f68
oyvsyo <lostedthare@gmail.com>
2016-08-28 21:04:00 +0300

    dkjdk

--- pagorb/settings.py
+++ pagorb/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (
