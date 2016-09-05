ce4a702436629e0c07f9205f072c888f71741c6c
oyvsyo <lostedthare@gmail.com>
2016-09-05 17:00:13 +0300

    add allowed hosts

--- pagorb/settings.py
+++ pagorb/settings.py
@@ -28,7 +28,7 @@ if os.environ['RUN_ENV'] != "DEV":
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'your mail here'
