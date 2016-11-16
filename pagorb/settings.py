2713f84c0afd35cc8eb3f373ce7343bb3480ae7e
oyvsyo <lostedthare@gmail.com>
2016-11-16 20:45:46 +0200

    pgsl

--- pagorb/settings.py
+++ pagorb/settings.py
@@ -86,10 +86,15 @@ WSGI_APPLICATION = 'pagorb.wsgi.application'
 # db_from_env = dj_database_url.config(conn_max_age=500)
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
+    # {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'yournewdb',
+    #     'USER': 'yournewuser',
+    #     'PASSWORD': 'whateverpasswordyouenteredearlier',
+    #     'HOST': '', # Set to empty string for localhost.
+    #     'PORT': '', # Set to empty string for default.
+    # }
 }
 # DATABASES['default'].update(db_from_env)
 
